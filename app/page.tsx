42cf18fdb07f10e2af68939bea2d5d51bf4650b3
Suyash <singhsuyash012@gmail.com>
2026-09-01 20:49:04 +0530

    preserve contact links in new section

--- app/page.tsx
+++ app/page.tsx
@@ -114,7 +114,7 @@ export default function Portfolio() {
 
         <section id="contact" className="section-shell contact-section">
           <div className="section-heading"><p className="eyebrow">05 / Contact</p><h2>Let&apos;s make something useful.</h2></div>
-          <div className="contact-row"><p className="lead-copy">Have a project, opportunity, or idea in mind? I&apos;d be glad to hear from you.</p><div className="contact-links"><a href="mailto:singhsuyash012@gmail.com">Email <ArrowUpRight size={15} /></a><a href="https://github.com/s4yashh" target="_blank" rel="noreferrer">GitHub <ArrowUpRight size={15} /></a><a href="https://www.linkedin.com/in/s4yashh/" target="_blank" rel="noreferrer">LinkedIn <ArrowUpRight size={15} /></a></div></div>
+          <div className="contact-row"><p className="lead-copy">Have a project, opportunity, or idea in mind? I&apos;d be glad to hear from you.</p><div className="contact-links"><a href="mailto:singhsuyash012@gmail.com">Email <ArrowUpRight size={15} /></a><a href="tel:+917985043880">+91 79850 43880 <ArrowUpRight size={15} /></a><a href="https://github.com/s4yashh" target="_blank" rel="noreferrer">GitHub <ArrowUpRight size={15} /></a><a href="https://www.linkedin.com/in/s4yashh/" target="_blank" rel="noreferrer">LinkedIn <ArrowUpRight size={15} /></a><a href="https://twitter.com/S4yash" target="_blank" rel="noreferrer">Twitter <ArrowUpRight size={15} /></a></div></div>
         </section>
       </main>
 
